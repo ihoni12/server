5be211faafa42602494b6f6b1f9b8cae9c6b8531
ihoni12 <ihonifeinberg@gmail.com>
2024-06-19 10:45:54 +0300

    aregla el doble leyo

--- TextPage/sumarThogether.js
+++ TextPage/sumarThogether.js
@@ -13,11 +13,14 @@ router.post('/leyoThogether', async (req, res) => {
             { $inc: { [`allSheferThogether.${index}`]: 1 } },
             { sort: { date: -1 }, new: true }
         );
+        /*
+        //enviar sumar 1 leyo, lla hay uno no es necesarios
         await AllIn.findOneAndUpdate(
             {},
             { $inc: { [`allSheferThogether.${index}`]: 1 } },
             { sort: { date: -1 }, new: true }
         );
+        */
         res.status(200).json({ success: true, msg: 'Actualización exitosa' });
     } catch (err) {
         res.status(500).json({ msg: 2 });
