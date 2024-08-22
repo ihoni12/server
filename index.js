7880e6f5b9ab35be0dfe2f9ae654eb389a097055
ihoni12 <ihonifeinberg@gmail.com>
2024-08-22 22:13:48 +0300

    separo cantidadLeyeron

--- index.js
+++ index.js
@@ -7,10 +7,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); // import cors package
-const socketIo = require('socket.io'); // Importa Socket.IO
 
-//const cantidadLeyeron = require('./socket/catidadLeyeron');//si los separo pone lo
-const AllIn = require('../models/AllIn');
+const cantidadLeyeron = require('./socket/catidadLeyeron');
 
 const authLogin = require('./LoginPage/authLogin');
 const authRegristro = require('./LoginPage/authRegristro');
@@ -64,60 +62,7 @@ mongoose
         const server = app.listen(PORT, () => {
             // Al conectarse
             console.log('Server is running on port ' + PORT);
-            //const io = cantidadLeyeron(server);
-
-            const io = socketIo(server);
-
-            let lastTotalCombined = null;
-
-            const calculateTotal = (field) => {
-                if (!field) return 0;
-                return Object.values(field).reduce(
-                    (acc, curr) => acc + curr,
-                    0
-                );
-            };
-
-            const emitData = async (nuevo = false) => {
-                try {
-                    // Consultar MongoDB para obtener el último documento
-                    const result = await AllIn?.findOne().sort({ date: -1 });
-                    if (result) {
-                        const totalAllSheferThogether = calculateTotal(
-                            result.allSheferThogether
-                        );
-
-                        // Emitir los datos solo si ha habido un cambio o es nuevo
-                        if (
-                            totalAllSheferThogether !== lastTotalCombined ||
-                            nuevo
-                        ) {
-                            const data = { count: totalAllSheferThogether };
-                            // Emitir a todos los clientes conectados
-                            io.emit('update', data);
-
-                            lastTotalCombined = totalAllSheferThogether;
-                        }
-                    }
-                } catch (err) {
-                    console.error('Error al consultar MongoDB:', err);
-                }
-            };
-
-            // Configurar un intervalo para emitir los datos a los clientes conectados
-            setInterval(emitData, 6000);
-
-            io.on('connection', (socket) => {
-                // Emitir los datos inmediatamente cuando un cliente se conecta
-                emitData(true);
-
-                // Limpiar al desconectar, si es necesario
-                /*socket.on('disconnect', () => {
-            console.log('Usuario desconectado: ' + socket.id);
-        });*/
-            });
-
-            return io;
+            const io = cantidadLeyeron(server);
         });
     })
     .catch((err) => {
