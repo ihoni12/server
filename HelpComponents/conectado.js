c62ec50f04e5032ed75ef541fca2cf8f139a2589
ihoni12 <ihonifeinberg@gmail.com>
2024-08-26 11:28:43 +0300

    conectado check devielve

--- HelpComponents/conectado.js
+++ HelpComponents/conectado.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Ruta de salud
-router.post('/conectadoCheck', (req, res) => {
+router.get('/conectadoCheck', (req, res) => {
     res.send('Server is running');
 });
 
