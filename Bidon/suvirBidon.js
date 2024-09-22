0217a33ee613665be101a0065851560f63cacc2d
ihoni12 <ihonifeinberg@gmail.com>
2024-09-22 17:03:57 +0300

    suma bidonim ccosas

--- Bidon/suvirBidon.js
+++ Bidon/suvirBidon.js
@@ -7,6 +7,9 @@ router.post('/nuevoBidon', async (req, res) => {
         //cambio 7
 
         const { key, newItem } = req.body;
+        console.log(key);
+        console.log(newItem);
+
         if (key === 'que') {
             return res.status(401).send({ msg: 'Key no valido' });
         }
@@ -55,6 +58,8 @@ router.post('/nuevoBidon', async (req, res) => {
         });
 
         await newBidon.save();
+        console.log('pasa');
+
         res.status(201).json({ msg: 'paso' }); //devuelvo lo que obtiene en 'savedUder' que es bueno, 201 = corecto
     } catch (err) {
         res.status(500).json({ error: err.message }); //si es un error devuelve el error
